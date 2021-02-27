924e3dcb2a6c140232020552668b7e560f029a09
xps-genesis <aruncool17@gmail.com>
2021-02-26 23:36:19 -0500

    1sec-500msec

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -33,19 +33,19 @@ class CarController():
     moving_fast = CS.out.vEgo > CS.CP.minSteerSpeed
 
     if enabled:
-      if self.timer < 100:
+      if self.timer < 50:
         self.timer += 1
       else:
-        self.timer = 100
+        self.timer = 50
     else:
       self.timer = 0
 
     if CS.out.vEgo > (CS.CP.minSteerSpeed - 0.5):  # for command high bit
-      self.gone_fast_yet = True if self.timer == 100 else False
+      self.gone_fast_yet = True if self.timer == 50 else False
     elif self.car_fingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.JEEP_CHEROKEE_2019):
       if CS.out.vEgo < (CS.CP.minSteerSpeed - 3.0):
         self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
-    lkas_active = moving_fast and enabled and (self.timer == 100)
+    lkas_active = moving_fast and enabled and (self.timer == 50)
 
     if not lkas_active:
       apply_steer = 0
