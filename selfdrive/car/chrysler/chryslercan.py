5bf06a71574f452b583d085f17a9987695333043
xps-genesis <aruncool17@gmail.com>
2021-03-04 21:41:08 -0500

    update

--- selfdrive/car/chrysler/chryslercan.py
+++ selfdrive/car/chrysler/chryslercan.py
@@ -43,7 +43,7 @@ def create_lkas_command(packer, apply_steer, enabled, moving_fast, frame):
   # LKAS_COMMAND 0x292 (658) Lane-keeping signal to turn the wheel.
   values = {
     "LKAS_STEERING_TORQUE": apply_steer,
-    "LKAS_HIGH_TORQUE": int(moving_fast & enabled),
+    "LKAS_HIGH_TORQUE": enabled,
     "COUNTER": frame % 0x10,
   }
   return packer.make_can_msg("LKAS_COMMAND", 0, values)
